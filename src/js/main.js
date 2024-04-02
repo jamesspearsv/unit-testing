355b463f27e57082d69bdcb9f4575b09da1c4c30
James Spears <jamesspearsv@gmail.com>
2024-04-01 21:52:18 -0400

    Complete capitalize, reverseString, calculator, and caesarCipher

--- src/js/main.js
+++ src/js/main.js
@@ -4,4 +4,80 @@ export function isTrue() {
   return true;
 }
 
-console.log('main.js');
+export function capitalize(string) {
+  let newString = '';
+
+  for (let i = 0; i < string.length; i++) {
+    const letter = i === 0 ? string[i].toUpperCase() : string[i];
+
+    newString += letter;
+  }
+
+  return newString;
+}
+
+export function reverseString(string) {
+  let newString = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+
+  return newString;
+}
+
+export const calculator = (() => {
+  function sum(a, b) {
+    return a + b;
+  }
+
+  function subtract(a, b) {
+    return a - b;
+  }
+
+  function multiply(a, b) {
+    return a * b;
+  }
+
+  function divide(a, b) {
+    return a / b;
+  }
+
+  return { sum, subtract, multiply, divide };
+})();
+
+export function caesarCipher(string, key) {
+  let numericString = [];
+  let newString = '';
+
+  if (key < 0) {
+    key += 26;
+  }
+
+  console.log(key);
+
+  for (let letter of string) {
+    let alphabet;
+    // Pick which alphabet to use based on letter case
+    if (letter.toUpperCase() === letter) {
+      alphabet = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZ'];
+    } else {
+      alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+    }
+
+    // find index of current letter
+    let index = alphabet.findIndex((value) => value === letter);
+
+    if (index === -1) {
+      newString += letter;
+      break;
+    }
+
+    // encode current letter
+    const encodedLetter = (index + key) % 26;
+    console.log(encodedLetter);
+    newString += alphabet[encodedLetter];
+  }
+
+  return newString;
+}
--- tests/main.test.js
+++ tests/main.test.js
@@ -1,6 +1,54 @@
-import { isTrue } from '../src/js/main';
+import {
+  caesarCipher,
+  calculator,
+  capitalize,
+  isTrue,
+  reverseString,
+} from '../src/js/main';
 
+// Control test to make sure jest is working
 test('isTrue', () => {
-  const test = isTrue();
-  expect(test).toBeTruthy();
+  const value = isTrue();
+  expect(value).toBeTruthy();
+});
+
+test('capitalize', () => {
+  const value = capitalize('cat');
+  expect(value).toBe('Cat');
+});
+
+test('reverseString', () => {
+  const value = reverseString('cat');
+  expect(value).toBe('tac');
+});
+
+test('calculator', () => {
+  const sum = calculator.sum(2, 3);
+  expect(sum).toBe(5);
+
+  const minus = calculator.subtract(5, 2);
+  expect(minus).toBe(3);
+
+  const product = calculator.multiply(5, 2);
+  expect(product).toBe(10);
+
+  const result = calculator.divide(15, 5);
+  expect(result).toBe(3);
+});
+
+test('cipher', () => {
+  const basic = caesarCipher('james', 1);
+  expect(basic).toBe('kbnft');
+
+  const wrap = caesarCipher('zebra', 5);
+  expect(wrap).toBe('ejgwf');
+
+  const caseSensitive = caesarCipher('James', 1);
+  expect(caseSensitive).toBe('Kbnft');
+
+  const negativeShift = caesarCipher('zebra', -4);
+  expect(negativeShift).toBe('vaxnw');
+
+  const nonletters = caesarCipher('Panic!', 2);
+  expect(nonletters).toBe('Rcpke!');
 });
